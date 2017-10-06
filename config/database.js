f9782a9d9021bda7f7d708461d263b1fb9094e89
Jacob Nelson <jacob_m_nelson@live.com>
2017-10-06 11:19:56 -0500

    Delete databse.js

--- config/database.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://jmnelson61199:Jakethesnake12@ds043917.mlab.com:43917/vidjot-prod'}
-} else {
-  module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'};
-}
