6b36aa8c80eb258ed54b7c7fab830afc71c06fcd
Jacob Nelson <jacob_m_nelson@live.com>
2017-10-06 11:31:27 -0500

    Create database.js

---
+++ config/database.js
@@ -0,0 +1,10 @@
+
+if (process.env.MONGODB_URI) {
+    module.exports = {
+        mongoURI: process.env.MONGODB_URI
+    };
+} else {
+    module.exports = {
+        mongoURI: 'mongodb://localhost/vidjot-dev'
+    };
+};
