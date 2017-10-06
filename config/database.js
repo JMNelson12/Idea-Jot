eb49d849f3ee9acbc96408f1610bfc4d11f24718
Jacob Nelson <jacob_m_nelson@live.com>
2017-10-06 11:12:42 -0500

    Fixed DB Connection Issue

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://<jmnelson61199>:<Jakethesnake12>@ds043917.mlab.com:43917/vidjot-prod'}
+  module.exports = {mongoURI: 'mongodb://jmnelson61199:Jakethesnake12@ds043917.mlab.com:43917/vidjot-prod'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'};
 }
