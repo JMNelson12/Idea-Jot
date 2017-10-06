d851da8f65329426cac8f8f3c6601bee5d3725ea
Jacob Nelson <jacob_m_nelson@live.com>
2017-10-06 11:31:11 -0500

    Mispelled

--- config/databse.js
+++
@@ -1,9 +0,0 @@
-if (process.env.MONGODB_URI) {
-    module.exports = {
-        mongoURI: process.env.MONGODB_URI
-    };
-} else {
-    module.exports = {
-        mongoURI: 'mongodb://localhost/vidjot-dev'
-    };
-};
